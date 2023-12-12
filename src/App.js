1107bc617ed552027cc02a44ac4f4b4f9ee25dd5
Lucasgermansierra <lucasgermansierra86@gmail.com>
2023-12-11 21:03:56 -0300

    Segunda Entrega

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/NavBar/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/NavBar/ItemDetailContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <ItemListContainer greeting={'Bienvenidos'}/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting={'Bienvenidos'} />} />
+          <Route path="/category/:categoryName" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,48 @@
-import CardWidget from "./CartWidget/CartWidget";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 
 const NavBar = () => {
-    return (
-        <nav>
-            <p class="fs-2">FUSION 3D</p>
-            <div>
-                <button type="button" class="btn btn-outline-primary">Inicio</button>
-                <button type="button" class="btn btn-outline-primary">Productos</button>
-                <button type="button" class="btn btn-outline-primary">Novedades</button>
-                <button type="button" class="btn btn-outline-primary">Ofertas</button>
-                <button type="button" class="btn btn-outline-primary">Contacto</button>
-            </div>
-            <CardWidget />
-        </nav>
-    )
-}
+  const cartItemCount = 3; // 
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        FUSION 3D
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarNav" />
+      <Navbar.Collapse id="navbarNav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/">
+            Inicio
+          </Nav.Link>
+          <Nav.Link as={Link} to="/category/1">
+            Productos
+          </Nav.Link>
+          {}
+          <Nav.Link as={Link} to="/category/2">
+            Novedades
+          </Nav.Link>
+          {}
+          <Nav.Link as={Link} to="/category/3">
+            Ofertas
+          </Nav.Link>
+          {}
+          <Nav.Link as={Link} to="/contacto">
+            Contacto
+          </Nav.Link>
+          {}
+          <Nav.Link as={Link} to="/item/123">
+            {}
+            <FaShoppingCart />
+            {cartItemCount > 0 && <span className="cart-item-count">{cartItemCount}</span>}
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default NavBar;
